ad630648e278992d5af66d622bc4cf550d6e9b8f
Hugo Rondet <hugo.rondet@yahoo.fr>
2024-05-14 14:08:56 +0200

    bdrt

--- 10_promise.js
+++ 10_promise.js
@@ -5,8 +5,21 @@
  * utilisez new Promise
  */
 
-const sleep = () => {
-
-};
+const attendreDeuxSecondes = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
+  };
+  
+  // Utilisation de la fonction asynchrone
+  const sleep = async() => {
+    console.log("Début de l'attente...");
+    await attendreDeuxSecondes();
+    console.log("Attente terminée !");
+  };
+  
+  sleep();
 
 module.exports = {sleep};
